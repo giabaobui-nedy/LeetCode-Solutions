f9817a8c5a2890570c3a44aa85ff2288c5b026ec
Gia Bao Bui <112450093+giabaobui-nedy@users.noreply.github.com>
2026-04-04 23:57:57 +1100

    Time: 0 ms (100%), Space: 19.9 MB (62.65%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,26 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        pointer = round((right - left) / 2)
+        while left < pointer < right:
+            print(left, pointer, right)
+            # binary search
+            if nums[pointer] > target:
+                right = pointer
+            elif nums[pointer] < target:
+                left = pointer
+            else:
+                return pointer
+            pointer = round(((right - left) / 2) + left)
+        if target == nums[left]:
+            return left
+        if target == nums[right]:
+            return right
+        if target < nums[left]:
+            return 0
+        if nums[left] < target < nums[right]:
+            return left + 1
+        if target > nums[right]:
+            return right + 1
+          
