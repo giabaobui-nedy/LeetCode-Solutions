a8a36b4778cb89d3a12fa7ca06245f68fd99f52a
Gia Bao Bui <112450093+giabaobui-nedy@users.noreply.github.com>
2026-05-03 11:55:43 +1000

    Time: 7 ms (29.31%), Space: 22 MB (11.37%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,5 @@
+from collections import Counter
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        unique = 0
-        for num in nums:
-            unique ^= num
-        return unique
-        
+        return Counter(nums).most_common()[-1][0]
+    
