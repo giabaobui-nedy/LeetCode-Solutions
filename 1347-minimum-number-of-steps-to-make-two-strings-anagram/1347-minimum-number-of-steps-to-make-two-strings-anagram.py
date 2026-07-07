53f6686f76e4f0cbcbb661c6c7eb3bb07a3d39e3
Gia Bao Bui <112450093+giabaobui-nedy@users.noreply.github.com>
2026-07-07 14:57:18 +1000

    Time: 66 ms (67.36%), Space: 19.7 MB (81.53%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -2,14 +2,6 @@ from collections import Counter
 
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        counter_s = Counter(s)
-        counter_t = Counter(t)
-        number_of_redundant_letters = 0
-        for letter in string.ascii_lowercase:
-            counter_of_that_letter_in_s = counter_s[letter]
-            counter_of_that_letter_in_t = counter_t[letter]
-            if counter_of_that_letter_in_t > counter_of_that_letter_in_s:
-                number_of_redundant_letters += counter_of_that_letter_in_t - counter_of_that_letter_in_s
-        return number_of_redundant_letters
+        return sum((Counter(t) - Counter(s)).values())
            
 
