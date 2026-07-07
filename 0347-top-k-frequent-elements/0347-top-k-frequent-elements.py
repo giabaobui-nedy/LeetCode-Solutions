fe042a2cf3432da2a84e72a3d14d33bab5800f64
Gia Bao Bui <112450093+giabaobui-nedy@users.noreply.github.com>
2026-07-08 06:03:26 +1000

    Time: 3 ms (89.44%), Space: 22.9 MB (41.69%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,5 +2,8 @@ from collections import Counter
 # import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        return [element for element, _ in Counter(nums).most_common(k)]
+        # Count frequencies of each number
+        counts = Counter(nums)
+        # Extract the k keys with the highest frequency values
+        return heapq.nlargest(k, counts, key=counts.get)
         
